529c6900e68c63bf8903f32b3a8847516c4439eb
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-07-09 18:52:14 +0100

    Updating image paths to enable build

--- components/page/header.vue
+++ components/page/header.vue
@@ -3,7 +3,7 @@
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
       <a href="https://threenine.co.uk" class="-m-1.5 p-1.5">
         <span class="sr-only">threenine.co.uk</span>
-        <nuxt-img class="h-16 w-auto" src="threenine-logo.png" alt="threenine.co.uk" />
+        <nuxt-img class="h-16 w-auto" src="/threenine-logo.png" alt="threenine.co.uk" />
       </a>
       <div class="flex lg:hidden">
         <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
@@ -21,7 +21,7 @@
         <div class="flex items-center justify-between">
           <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">threenine.co.uk</span>
-            <nuxt-img class="h-10 w-auto" src="threenine.svg" alt="threenine.co.uk" />
+            <nuxt-img class="h-10 w-auto" src="/threenine.svg" alt="threenine.co.uk" />
           </a>
           <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
             <span class="sr-only">Close menu</span>
