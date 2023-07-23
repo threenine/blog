4f294fda955da37cacd1aa2dbe2b1532b6e6002f
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-07-23 18:40:28 +0100

    Updated content

---
+++ components/content/quote.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+
+    <div class="w-full mx-auto rounded-lg bg-gray-100 shadow-lg px-5 pt-5 pb-10 text-gray-900" >
+      <div class="w-full mb-10">
+        <div class="text-3xl text-orange-500 text-left leading-tight h-3">“</div>
+        <p class="text-2xl text-gray-700 text-center px-5"><slot/></p>
+        <div class="text-3xl text-orange-500 text-right leading-tight h-3 -mt-3">”</div>
+      </div>
+
+
+  </div>
+
+</template>
+
+<style scoped></style>
