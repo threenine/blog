ab58a0a3278c4e318367ccc79803036bd75781b2
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-07-24 22:59:45 +0100

    Updated what is rest api content

--- components/content/warning.vue
+++ components/content/warning.vue
@@ -2,9 +2,9 @@
 
 <template>
   <div
-      class="mb-4 rounded-lg bg-yellow-200 px-6 py-5 text-base text-gray-900"
+      class="mb-4 rounded-lg bg-yellow-100 px-6 py-5 text-base text-gray-800"
       role="alert">
-    <h4 class="mb-2 text-2xl font-medium leading-tight"><slot name="title" /></h4>
+    <h4 class="mb-2 text-2xl font-medium leading-tight text-center"><slot name="title" /></h4>
     <div class="mb-4">
     <slot />
     </div>
