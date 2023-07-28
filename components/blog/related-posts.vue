b7d2826433e66686bc8f2a4b19536f09f8526609
gary <gary.woodfine@threenine.co.uk>
2023-07-28 08:48:53 +0100

    initial related post component

--- components/blog/related-posts.vue
+++ components/blog/related-posts.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 })
 const {data: posts} = await useAsyncData('posts', () => queryContent('/posts')
      .where({category: props.category})
-    .sort({publishDateTime: -1})
+    .sort({publishDateTime: 1})
     .limit(4)
     .only(['_id','_path', 'title'])
     .find())
@@ -19,7 +19,7 @@ const {data: posts} = await useAsyncData('posts', () => queryContent('/posts')
       <p class="text-lg text-orange-500 ml-5 text=semibold mb-5 mt-5">Related Posts</p>
       <ul role="list" class="divide-y divide-gray-300 divide-dashed">
         <li v-for="post in posts" :key="post._id" class="px-6 py-4">
-          <nuxt-link :to="post._path" class="text-orange-400 text-base">{{post.title}}</nuxt-link>
+          <nuxt-link :to="post._path" class="text-orange-400 text-base hover:underline">{{post.title}}</nuxt-link>
         </li>
       </ul>
     </div>
