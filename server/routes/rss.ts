46fbdf3782b338b47dda8e77beaf68208b097285
Gary Woodfine <lnb0l9dc@duck.com>
2023-09-22 09:10:45 +0100

    updated Rss Feed

--- server/routes/rss.ts
+++ server/routes/rss.ts
@@ -6,9 +6,12 @@ export default defineEventHandler(async (event) => {
     const feed = new RSS({
         title: 'threenine',
         description: 'UK Based Software Development consultancy specialising in Back-end development with Cloud Native platforms ',
-        webMaster: 'threenine.co.uk',
+        webMaster: 'Gary Woodfine',
+        copyright: ` ${new Date().getFullYear()} threenine.co.uk ` ,
         site_url: 'https://threenine.blog',
         feed_url: `https://threenine.blog/rss`,
+        language: 'en',
+        categories: ['API Development', 'Software Consultancy']
     });
 
     const docs = await serverQueryContent(event)
@@ -30,4 +33,6 @@ export default defineEventHandler(async (event) => {
     event.node.res
     event.node.res.setHeader('content-type', 'text/xml');
     event.node.res.end(feedString);
+
+
 });
